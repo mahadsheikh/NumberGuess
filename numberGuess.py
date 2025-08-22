64cbd37c7b26815f92c5735b6ecafd95385a583e
Mahad Sheikh <sheikhmahad17@gmail.com>
2025-08-22 16:44:02 -0400

    addding guess

--- numberGuess.py
+++ numberGuess.py
@@ -1,7 +1,6 @@
 import random
 
 top_range = input("Type a Number : ")
-
 if top_range.isdigit():
     top_range = int(top_range)
     if top_range < 0 :
@@ -10,4 +9,20 @@ else:
     print("Please type a number ")
 
 random_number = random.randint(0,top_range)
-print(random_number)
+guesses = 0
+while True:
+    guesses += 1
+    usr_guess = input("make a guess : ")
+    if usr_guess.isdigit():
+        usr_guess = int(usr_guess)
+    else:
+        print("Please type a number ")
+        continue
+
+    if usr_guess == random_number:
+        print("You guessed it")
+        break
+    else:
+        print("You guessed incorrect")
+
+print("you got it in ",guesses," guesses")
